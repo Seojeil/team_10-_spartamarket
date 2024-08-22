592756303a4c04c57f8dcf35c5a1a06d9bf2b6ed
jung-sungwon <pompous89@gmail.com>
2024-08-22 10:29:51 +0900

    회원가입 완료 /데이터베이스 저장확인 관리자 페이지에서 회원가입 확인 할수있음 관리자 id :  admin1 pw:  admin1234 직접 아이디 만들어 보시고 잘 되는지 확인 부탁드립니다

--- spartamarket/accounts/models.py
+++ spartamarket/accounts/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     pass
+
+
+# class Signup(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
--- spartamarket/accounts/views.py
+++ spartamarket/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from .forms import SignUpForm
 
-# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect("index")
+        else:
+            # 폼 오류 메시지를 콘솔에 출력
+            print(form.errors)
+    else:
+        form = SignUpForm()
+    context = {"form": form}
+    return render(request, "accounts/signup.html", context)
