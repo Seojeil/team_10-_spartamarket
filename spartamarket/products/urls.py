03a457f7d08cd86308ce04abfd8086ceae788f95
jung-sungwon <pompous89@gmail.com>
2024-08-26 10:34:44 +0900

    메인페이지 상품가격 생성

--- spartamarket/products/urls.py
+++ spartamarket/products/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('<int:pk>/delete/', views.delete, name='delete'),
     path('<int:pk>/update/', views.update, name='update'),
     path('<int:pk>/comments/', views.comments, name='comments'),
-    path('<int:pk>/comments_delete/', views.comments_delete, name='comments_delete'),
+    path('<int:pk>/comments_delete/',
+         views.comments_delete, name='comments_delete'),
     path('<int:pk>/like/', views.like, name='like'),
 ]
