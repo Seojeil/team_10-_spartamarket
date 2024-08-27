5371adabaa8a0b80f7c485b3a8ca8c125afa3146
jeil <sjl01002@gmail.com>
2024-08-27 09:25:31 +0900

    fix:gitignore수정 등

--- spartamarket/accounts/models.py
+++ spartamarket/accounts/models.py
@@ -5,7 +5,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
-    followings = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name='followers')
     
     def __str__(self):
         return f'{self.username} Profile'
